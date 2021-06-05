c0a6effa9783c31e4dc668e7a0f2cd333ece15c3
Nicholas Mychka <nickmychka@gmail.com>
2021-06-05 23:12:00 +0300

    test with one route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :boards, only: [:index, :create]
+  # resources :boards, only: [:index, :create]
 
   root 'welcome#index'
 end
